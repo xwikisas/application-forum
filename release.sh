4b3747307aa30abfdb2f7aff73109dba061e91a9
Marius Dumitru Florea <marius@xwiki.com>
2024-02-08 10:09:32 +0200

    [misc] Fix docker test module version.

--- release.sh
+++ release.sh
@@ -20,4 +20,4 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 # ---------------------------------------------------------------------------
 
-mvn release:prepare -U -DautoVersionSubmodules=true -Pintegration-tests,quality -DskipTests=true -Darguments="-DskipTests=true" && mvn release:perform -Pintegration-tests,quality -DskipTests=true -Darguments="-DskipTests=true"
+mvn release:prepare -U -DautoVersionSubmodules=true -Pintegration-tests,docker,quality -DskipTests=true -Darguments="-DskipTests=true" && mvn release:perform -Pintegration-tests,docker,quality -DskipTests=true -Darguments="-DskipTests=true"
